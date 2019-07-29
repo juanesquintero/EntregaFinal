9cec434f4cb2367be9461afd85f5e405a43aee4a
juanesquintero <juaneschrome@gmali.com>
2019-07-29 17:54:17 -0500

    delete SendGrid API Key

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
 process.env.PORT = process.env.PORT || 3000
 process.env.URI = process.env.URI || 'mongodb://localhost:27017/educacionContinua' 
-process.env.SENDGRID_API_KEY = process.env.SENDGRID_API_KEY || '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'
+process.env.SENDGRID_API_KEY = process.env.SENDGRID_API_KEY 
