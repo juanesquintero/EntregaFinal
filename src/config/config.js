d1caaf78a554e3c6abfb38194089d70d0757435e
juanesquintero <juaneschrome@gmali.com>
2019-07-29 12:28:04 -0500

    sengrid

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
 process.env.PORT = process.env.PORT || 3000
 process.env.URI = process.env.URI || 'mongodb://localhost:27017/educacionContinua' 
-process.env.SENDGRID_API_KEY = process.env.SENDGRID_API_KEY ||  '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' 
+process.env.SENDGRID_API_KEY = process.env.SENDGRID_API_KEY 
